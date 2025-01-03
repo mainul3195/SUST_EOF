83a2057bbdb0792bc8588ab644b14c34812c33a3
Alve-2019331050 <69475730+Alve-2019331050@users.noreply.github.com>
2025-01-03 11:00:34 +0600

    Update pallindromic tree.cpp

--- String/pallindromic tree.cpp
+++ String/pallindromic tree.cpp
@@ -1,32 +1,26 @@
-struct PalindromicTree {struct node {
-int nxt[26], len, st, en, link, diff, slink, cnt, oc;};
-string s;vector<node> t;int sz, last;PalindromicTree() {}
-PalindromicTree(string _s) {s = _s;int n = s.size();t.clear();
-t.resize(n + 9);sz = 2, last = 2;t[1].len = -1, t[1].link = 1;
-t[2].len = 0, t[2].link = 1;t[1].diff = t[2].diff = 0;
-t[1].slink = 1;t[2].slink = 2;}int extend(int pos) { 
-// returns 1 if it creates a new palindrome
-int cur = last, curlen = 0;int ch = s[pos] - 'a';
-while (1) {curlen = t[cur].len;
-if (pos - 1 - curlen >= 0 && s[pos - 1 - curlen] == s[pos]) break;cur = t[cur].link;
-}if (t[cur].nxt[ch]) {last = t[cur].nxt[ch];t[last].oc++;return 0;}
-sz++;last = sz;t[sz].oc = 1;t[sz].len = t[cur].len + 2;t[cur].nxt[ch] = sz;
-t[sz].en = pos;t[sz].st = pos - t[sz].len + 1;
-if (t[sz].len == 1) {t[sz].link = 2;t[sz].cnt = 1;
-t[sz].diff = 1;t[sz].slink = 2;return 1;}
-while (1) {cur = t[cur].link;curlen = t[cur].len;
-if (pos - 1 - curlen >= 0 && s[pos - 1 - curlen] == s[pos]) {
-t[sz].link = t[cur].nxt[ch];break;}}t[sz].cnt = 1 + t[t[sz].link].cnt;
-t[sz].diff = t[sz].len - t[t[sz].link].len;if (t[sz].diff == t[t[sz].link].diff) t[sz].slink = t[t[sz].link].slink;
-else t[sz].slink = t[sz].link;return 1;}void calc_occurrences() {
-for (int i = sz; i >= 3; i--) t[t[i].link].oc += t[i].oc;
-}vector<array<int, 2>> minimum_partition() { //(even, odd), 1 indexed
-int n = s.size();vector<array<int, 2>> ans(n + 1, {0, 0}), series_ans(n + 5, {0, 0});
-ans[0][1] = series_ans[2][1] = 1e9;for (int i = 1; i <= n; i++) {
-extend(i - 1);for (int k = 0; k < 2; k++) {ans[i][k] = 1e9;
-for (int v = last; t[v].len > 0; v = t[v].slink) {series_ans[v][!k] = ans[i - (t[t[v].slink].len + t[v].diff)][!k];
-if (t[v].diff == t[t[v].link].diff) series_ans[v][!k] = min(series_ans[v][!k], series_ans[t[v].link][!k]);
-ans[i][k] = min(ans[i][k], series_ans[v][!k] + 1);}}}return ans;}} t;
-int32_t main() {ios_base::sync_with_stdio(0);cin.tie(0);string s;cin >> s;PalindromicTree t(s);
-for (int i = 0; i < s.size(); i++) t.extend(i);t.calc_occurrences();long long ans = 0;
-for (int i = 3; i <= t.sz; i++) ans += t.t[i].oc;cout << ans << '\n';return 0;}
+// s = "#" + s; 
+struct PaliTree{ #define sz 26 struct node{ int  
+lng; int link; int next[sz]; int occ; node(int _ 
+lng){ lng = _lng; link = 0; occ = 0; memset(next 
+,-1,sizeof(next)); } }; vector<node> tree; strin 
+g s; int cur;  PaliTree(){ tree.push_back(node(- 
+1)); //img 
+tree.push_back(node(0)); //root 
+cur = 1; } void clear(){ tree.clear(); tree.push 
+_back(node(-1)); //img 
+tree.push_back(node(0)); //root 
+cur = 1; } int get_id(char c){return c-'a';} int 
+get_link(int now,int i){ char c = s[i]; while(1 
+){ if(now == 0 or (i-1-tree[now].lng > 0 and s[i -1-tree[now].lng] == c)) break; now = tree[now].
+ link; } int id = get_id(c); return (tree[now].ne
+ xt[id] == -1)?now:tree[now].next[id]; } void add
+ (int i){ char c = s[i]; int id = get_id(c); whil
+ e(1){ if(cur == 0 or (i-1-tree[cur].lng > 0 and
+ s[i-1-tree[cur].lng] == c)) break; cur = tree[cu
+ r].link; } if(cur == 0 and s[i] == s[i-1]) cur =
+ 1; if(tree[cur].next[id] == -1){ node tmp(tree[ 
+cur].lng+2); if(tmp.lng == 1) tmp.link = 1; else 
+tmp.link = get_link(tree[cur].link,i); tree.pus 
+h_back(tmp); tree[cur].next[id] = tree.size()-1; 
+} cur = tree[cur].next[id]; tree[cur].occ++; }  
+void calc(){ for(int i = tree.size()-1; i > 1; i --) tree[tree[i].link].occ += tree[i].occ; } };
